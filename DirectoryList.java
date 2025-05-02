811c4bad0412413a59f6820fdcdb3e5ebb2b5bbf
Fathima <fidhafathima2069@gmail.com>
2025-05-02 16:08:40 +0530

    CO6 PROGRAM

---
+++ DirectoryList.java
@@ -0,0 +1,29 @@
+import java.io.File;
+
+public class DirectoryList {
+    public static void main(String[] args) {
+        String dirPath = "C:\\Program Files";
+        listDirectoryContents(dirPath);
+    }
+
+    public static void listDirectoryContents(String dirPath) {
+        File directory = new File(dirPath);
+
+        if (directory.exists() && directory.isDirectory()) {
+            System.out.println("Listing subdirectories and files in: " + dirPath);
+            File[] directoryContents = directory.listFiles();
+
+            if (directoryContents != null) {
+                for (File item : directoryContents) {
+                    if (item.isDirectory()) {
+                        System.out.println("Directory: " + item.getName());
+                    } else {
+                        System.out.println("File: " + item.getName());
+                    }
+                }
+            }
+        } else {
+            System.out.println("Directory does not exist or is not a directory.");
+        }
+    }
+}
---
+++ ReadWriteDisp.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadWriteDisp {
+    public static void main(String[] args) {
+        String fileName, content;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Name of File: ");
+        fileName = sc.nextLine();
+
+        // Write to the file
+        try {
+            FileWriter f1 = new FileWriter(fileName);
+            System.out.println("\nEnter the Content (Press Ctrl+Z then Enter to stop):");
+
+            while (sc.hasNextLine()) {
+                content = sc.nextLine();
+                f1.write(content + "\n");
+            }
+
+            f1.close();
+            System.out.println("\nContent written to the file successfully.");
+        } catch (IOException ioe) {
+            System.out.println("\nException while writing: " + ioe);
+        }
+
+        // Read from the file
+        try {
+            FileReader fr = new FileReader(fileName);
+            Scanner fileScanner = new Scanner(fr);
+
+            System.out.println("\nContent of the file:");
+            while (fileScanner.hasNextLine()) {
+                content = fileScanner.nextLine();
+                System.out.println(content);
+            }
+
+            fileScanner.close();
+            fr.close();
+        } catch (IOException ioe) {
+            System.out.println("\nException while reading: " + ioe);
+        }
+
+        sc.close(); // Close Scanner
+    }
+}
